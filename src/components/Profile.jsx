807837aac2fb73cfd57cc142de75e4f5703f11e8
Shehriyar Saleem <127856369+Shehryar-dev@users.noreply.github.com>
2025-06-15 01:54:17 +0500

    Code Updated!!

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,7 +7,7 @@ function Profile() {
     if (!user) {
         return (
             <div className="flex items-center justify-center min-h-screen text-black bg-gray-100">
-                <div className="bg-white p-6 rounded-lg shadow-md text-center text-gray-700 text-xl font-semibold">
+                <div className="bg-white p-6 rounded-lg shadow-md text-center text-black text-xl font-semibold" >
                     Please login to view your profile.
                 </div>
             </div>
